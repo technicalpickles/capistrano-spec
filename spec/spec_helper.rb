b4ce66b09dde56772cf7c04eb74816eb860cee3a
Karl Matthias <karl@matthias.org>
2012-04-04 10:00:22 +0100

    Modernized to work with Rspec 2.*

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'capistrano-spec'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 
 Spec::Runner.configure do |config|
   
