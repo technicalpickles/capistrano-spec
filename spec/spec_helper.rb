0dbca00a2ed60e816124270b2fbc80ca850ceb47
petems <p.morsou@gmail.com>
2013-03-27 22:37:25 +0000

    Added spec for main module matchers and helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'capistrano'
 require 'capistrano-spec'
 require 'rspec'
 require 'rspec/autorun'
 
 RSpec.configure do |config|
-
+	config.include Capistrano::Spec::Matchers
+	config.include Capistrano::Spec::Helpers
 end
