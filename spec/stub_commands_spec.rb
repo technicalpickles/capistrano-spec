93589794373d2bc04131dd00684fd37107317517
Jan Graichen <jg@altimos.de>
2013-04-07 15:47:34 +0200

    Add standalone specs for stubbing commands.

---
+++ spec/stub_commands_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+require 'capistrano'
+
+describe 'Command stubbing' do
+  before do
+    @configuration = Capistrano::Configuration.new
+    @configuration.extend Capistrano::Spec::ConfigurationExtension
+    @configuration.load do
+      def remote_pwd
+        capture 'pwd'
+      end
+
+      def remote_sudo_pwd
+        capture 'pwd', :via => :sudo
+      end
+
+      def custom_pwd
+        run 'pwd' do |ch, stream, data|
+          return "#{stream}: #{data}"
+        end
+      end
+    end
+  end
+
+  it 'should allow to stub command output' do
+    @configuration.stub_command 'pwd', data: '/stubded/path'
+    @configuration.remote_pwd.should == '/stubded/path'
+  end
+
+  it 'should allow to stub sudo command output' do
+    @configuration.stub_command "sudo -p 'sudo password: ' pwd", data: '/stubbed/path'
+    @configuration.remote_sudo_pwd.should == '/stubbed/path'
+  end
+
+  it 'should allow to stub custom command output' do
+    @configuration.stub_command 'pwd', data: '/stubbed/path'
+    @configuration.custom_pwd.should == 'out: /stubbed/path'
+  end
+
+  it 'should allow to stub stream' do
+    @configuration.stub_command 'pwd', data: '/stubbed/path', stream: :err
+    @configuration.custom_pwd.should == 'err: /stubbed/path'
+  end
+end
