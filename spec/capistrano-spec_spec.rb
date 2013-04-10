5300f9058b67a53353bd4f88c4375a99482bfdcd
petems <p.morsou@gmail.com>
2013-04-10 13:50:59 +0100

    Fix RSpec syntax

--- spec/capistrano-spec_spec.rb
+++ spec/capistrano-spec_spec.rb
@@ -18,56 +18,68 @@ describe Capistrano::Spec do
       foo = double(foo)
     end
 
-    it "should have a #callback matcher" do
+    it "has a #callback matcher" do
       expect{@configuration.should callback(foo)}.to_not raise_error(NoMethodError)
     end
 
-    it "should have a #have_uploaded matcher" do
+    it "has a #have_uploaded matcher" do
       expect{@configuration.should have_uploaded(foo)}.to_not raise_error(NoMethodError)
     end
 
-    it "should have a #have_run matcher" do
+    it "has a #have_run matcher" do
       expect{@configuration.should have_run(foo)}.to_not raise_error(NoMethodError)
     end
 
-    it "#have_run will not raise error when run is in recipe" do
+  end
+
+  describe 'have_run' do
+
+    it "will not raise error when run is in recipe" do
       @configuration.find_and_execute_task('fake:thing')
       expect{@configuration.should have_run("do some stuff")}.to_not raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to run .*\s*, but did not/)
     end
 
-    it "#have_run will raise error when run not in recipe" do
+    it "will raise error when run not in recipe" do
       @configuration.find_and_execute_task('fake:thing')
       expect{@configuration.should have_run("don't find me")}.to raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to run .*\s*, but did not/)
     end
 
-    it "#have_uploaded will not raise error when upload is in recipe" do
+  end
+
+  describe 'have_uploaded' do
+
+    it "will not raise error when upload is in recipe" do
       @configuration.find_and_execute_task('fake:thing')
       expect{@configuration.should have_uploaded('foo').to('/tmp/foo')}.to_not raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to upload .*\s* to .*\s* but did not/)
     end
 
-    it "#have_uploaded will raise error when run upload not in recipe" do
+    it "will raise error when run upload not in recipe" do
       @configuration.find_and_execute_task('fake:thing')
       expect{@configuration.should have_uploaded('bar').to('/tmp/bar')}.to raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to upload .*\s* to .*\s* but did not/)
     end
+  end
 
-    it "#have_gotten will not raise error when get is in recipe" do
+  describe 'have_gotten' do
+    it "will not raise error when get is in recipe" do
       @configuration.find_and_execute_task('fake:thing')
       expect{@configuration.should have_gotten('/tmp/baz').to('baz')}.to_not raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to get .*\s* to .*\s* but did not/)
     end
 
-    it "#have_gotten will raise error when get not in recipe" do
+    it "will raise error when get not in recipe" do
       @configuration.find_and_execute_task('fake:thing')
       expect{@configuration.should have_gotten('/tmp/blegga').to('blegga')}.to raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to get .*\s* to .*\s* but did not/)
     end
+  end
 
-    it "#callback will not raise error when `before` callback has occured" do
+  describe 'callback' do
+
+    it "will not raise error when `before` callback has occured" do
       expect{@configuration.should callback('fake:thing').before('fake:stuff_and_things')}.to_not raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to callback .*\s* before .*\s*, but did not/)
     end
 
-    it "#callback will not raise error when `after` callback has occured" do
+    it "will not raise error when `after` callback has occured" do
       expect{@configuration.should callback('fake:other_thing').after('fake:thing')}.to_not raise_error(RSpec::Expectations::ExpectationNotMetError, /expected configuration to callback .*\s* after .*\s*, but did not/)
     end
-
   end
 
 end
